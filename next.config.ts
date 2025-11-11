e55b65bc751d017b56f3018b0a868dd5cd375069
Dorian Musaj <dorianmusaj@gmail.com>
2025-11-11 23:04:22 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    output: 'export',
-  basePath: '',
+  basePath: '/teamway-website',
   images: { unoptimized: true }
 };
 
